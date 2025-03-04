e6a5a811113056aedb3291af367d31a5f395622f
Charlie Hooey <charleshooey@gmail.com>
2025-03-04 16:03:45 -0500

    Lab period over

--- Lab7code.R
+++ Lab7code.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(e1071)
 ######Part 1########
 
 
@@ -55,14 +56,62 @@ beta.moment <- function(alpha, beta, k, centered=TRUE){
 }
 
 ####stats for (a=2, b=5)
-25_stats <- tibble(
-mean = beta.moment(2,5,1,centered=FALSE),
-variance = beta.moment(2,5,2,centered=TRUE),
-skewness = (beta.moment(2,5,3,centered=TRUE))/((beta.moment(2,5,2,centered=TRUE))^(3/2)),
-ex_kurt = (beta.moment(2,5,4,centered=TRUE))/(beta.moment(2,5,2,centered=TRUE))^2)
 
-
+stats25 <- tibble(
+  mean = beta.moment(2,5,1,centered=FALSE),
+  variance = beta.moment(2,5,2,centered=TRUE),
+  skewness = (beta.moment(2,5,3,centered=TRUE))/((beta.moment(2,5,2,centered=TRUE))^(3/2)),
+  ex_kurt = ((beta.moment(2,5,4,centered=TRUE))/((beta.moment(2,5,2,centered=TRUE))^2))-3
+  
 )
+
+stats55 <- tibble(
+  mean = beta.moment(2,5,1,centered=FALSE),
+  variance = beta.moment(2,5,2,centered=TRUE),
+  skewness = (beta.moment(2,5,3,centered=TRUE))/((beta.moment(2,5,2,centered=TRUE))^(3/2)),
+  ex_kurt = ((beta.moment(2,5,4,centered=TRUE))/((beta.moment(2,5,2,centered=TRUE))^2))-3
+  
+)
+
+stats.5 <- tibble(
+  mean = beta.moment(2,5,1,centered=FALSE),
+  variance = beta.moment(2,5,2,centered=TRUE),
+  skewness = (beta.moment(2,5,3,centered=TRUE))/((beta.moment(2,5,2,centered=TRUE))^(3/2)),
+  ex_kurt = ((beta.moment(2,5,4,centered=TRUE))/((beta.moment(2,5,2,centered=TRUE))^2))-3
+  
+)
+
+##################Task 3##########
+graph_funct <- function(alpha, beta){
+  set.seed(7272) # Set seed so we all get the same results.
+  sample.size <- 500 # Specify sample details
+  beta.sample <- rbeta(n = sample.size, # sample size
+                       shape1 = alpha, # alpha parameter
+                       shape2 = beta) #beta
+  
+  tibble(x=beta.sample) |>
+    summarize(
+      mean = mean(x),
+      variance = var(x),
+      skewness = skewness(x),
+      ex_kurt = (kurtosis(x))
+    )
+  
+  histogram <- hist(beta.sample)
+  histogram 
+  
+
+  ggplot(data = beta.sample)+
+  geom_density
+    
+}
+
+
+
+
+
+
+
 
 
 
